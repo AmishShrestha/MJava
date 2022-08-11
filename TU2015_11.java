a7d17f009b88d16572077135d90f9ae9819e6597
AmishShrestha <109086386+AmishShrestha@users.noreply.github.com>
2022-08-11 13:23:37 +0545

    TU 2015, Ques 11

---
+++ TU2015_11.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+
+//Display even num from 1 to 500.
+
+public class TU2015_11 {
+    public static void main(String[] args) {
+        for(int i=0;i<=500;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
